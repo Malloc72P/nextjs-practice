70067a60416bdaad2ef2620674d374e683d3d7ed
Malloc72P <scra1028@gmail.com>
2022-06-08 21:21:47 +0900

    누락된 파일 커밋함

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type {NextPage} from 'next'
+import Layout from "../components/layout/layout";
+import styles from "../styles/pages/index.module.css"
+
+const Home: NextPage = () => {
+  return (
+    <Layout>
+      <h1>Testing</h1>
+    </Layout>
+  )
+}
+
+export default Home
